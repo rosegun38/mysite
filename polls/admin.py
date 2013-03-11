8804f26ddb052445566c7f43c68f0af6937cd01f
Jian Ma <rosegun38@gmail.com>
2013-03-11 13:42:52 +0800

    Initial contents of test project

---
+++ polls/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from polls.models import Poll
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+	
+admin.site.register(Poll,PollAdmin)
